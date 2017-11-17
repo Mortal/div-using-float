73970134756abefed3342825674fe7dca5042a47
Mathias Rav <rav@cs.au.dk>
2017-11-17 13:52:20 +0100

    Serialize

--- arraydiv.c
+++ arraydiv.c
@@ -1,6 +1,8 @@
-void intdiv(const unsigned int *n1, const unsigned int *n2, const unsigned int *d1, unsigned int *q1) {
-	while (n1 != n2) *q1++ = *n1++ / *d1++;
+void intdiv(const unsigned int *n1, const unsigned int *n2, unsigned int *q1) {
+	unsigned int d = 42;
+	while (n1 != n2) d = ~(*q1++ = *n1++ / d);
 }
-void floatdiv(const unsigned int *n1, const unsigned int *n2, const unsigned int *d1, unsigned int *q1) {
-	while (n1 != n2) *q1++ = (unsigned int) (((double) *n1++) / ((double) *d1++));
+void floatdiv(const unsigned int *n1, const unsigned int *n2, unsigned int *q1) {
+	unsigned int d = 42;
+	while (n1 != n2) d = ~(*q1++ = (unsigned int) (((double) *n1++) / ((double) d)));
 }
